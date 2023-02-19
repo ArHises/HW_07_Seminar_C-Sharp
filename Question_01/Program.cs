92f5c613407636fc128e0e16c03d5775c9c341db
Arhises <roma1039@gmail.com>
2023-02-19 18:57:30 +0200

    First q added

---
+++ Question_01/Program.cs
@@ -0,0 +1,54 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+int[,] CreateIntTable()
+{
+    Console.Write("Lines: ");
+    int line = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Colons: ");
+    int colon = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+    int[,] table = new int[line, colon];
+    return table;
+}
+int[,] FillTable(int[,] table)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return table;
+}
+void PrintTable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(" " + table[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+void AverageOfColon(int[,] table)
+{
+    int sum = 0;
+    for (int i = 0; i < table.GetLength(1); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < table.GetLength(0); j++)
+        {
+            sum += table[j,i];
+        }
+        Console.WriteLine($"Average of colon {i + 1} = " + Convert.ToDouble(sum) / table.GetLength(1));
+    }
+}
+int[,] table = CreateIntTable();
+FillTable(table);
+PrintTable(table);
+AverageOfColon(table);
